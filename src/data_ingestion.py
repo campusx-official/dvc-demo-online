d28775b6ede4502a9fc5b85c881be4223cd70df7
nitishksingh24 <nitishksingh24@gmail.com>
2024-06-26 01:49:51 +0530

    add second exp

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,6 +6,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/mast
 
 df = df.iloc[:, 3:]
 
-df = df[df['Length of Membership'] > 3]
+df = df[df['Length of Membership'] > 1]
 
+df.drop(columns=['Avg. Session Length'],inplace=True)
 df.to_csv(os.path.join('data','customer.csv'))
