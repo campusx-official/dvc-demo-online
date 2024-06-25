27fe872bf795e6ff75fb053bfe6c361caf5b6bad
nitishksingh24 <nitishksingh24@gmail.com>
2024-06-26 01:32:47 +0530

    add first experiment

---
+++ src/data_ingestion.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import os
+
+df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
+
+df = df.iloc[:, 3:]
+
+df = df[df['Length of Membership'] > 3]
+
+df.to_csv(os.path.join('data','customer.csv'))
